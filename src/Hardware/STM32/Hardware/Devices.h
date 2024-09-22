ef6b49878caf54782610a4adb9c4da805eea8273
Andy Shaw <andy-git@gloomy-place.com>
2024-09-22 13:46:39 +0100

    Cleanup AsyncSerial usage

--- src/Hardware/STM32/Hardware/Devices.h
+++ src/Hardware/STM32/Hardware/Devices.h
@@ -2,7 +2,6 @@
 #define SRC_STM32_HARDWARE_DEVICES_H_
 
 #include <AsyncSerial.h>
-typedef AsyncSerial UARTClass;
 #define SUPPORT_USB		1		// needed by USBSerial.h
 #include <USBSerial.h>
 extern USBSerial serialUSB;
