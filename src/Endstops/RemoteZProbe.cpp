43503d238d0222e5482094c7a343b25c03156580
Andy Shaw <andy-git@gloomy-place.com>
2025-03-08 22:58:19 +0000

    Fix for incorrect initial probe state (possible temporary)

--- src/Endstops/RemoteZProbe.cpp
+++ src/Endstops/RemoteZProbe.cpp
@@ -111,8 +111,9 @@ GCodeResult RemoteZProbe::Create(const StringRef& pinNames, const StringRef& rep
 
 	RemoteInputHandle h;
 	h.Set(RemoteInputHandle::typeZprobe, number, 0);
+	bool state = false;
 	const uint16_t threshold = (type == ZProbeType::scanningAnalog) ? DefaultZProbeADValue : 0;		// nonzero threshold makes it an analog handle
-	const GCodeResult rc = CanInterface::CreateHandle(boardAddress, h, pinNames.c_str(), threshold, ActiveProbeReportInterval, nullptr, reply);
+	const GCodeResult rc = CanInterface::CreateHandle(boardAddress, h, pinNames.c_str(), threshold, ActiveProbeReportInterval, &state, reply);
 	if (rc < GCodeResult::error)						// don't set the handle unless it is valid, or we will get an error when this probe is deleted
 	{
 		handle = h;
@@ -121,6 +122,8 @@ GCodeResult RemoteZProbe::Create(const StringRef& pinNames, const StringRef& rep
 			float dummyValue;
 			(void)GetCalibratedReading(dummyValue);				// get an initial reading for the object model
 		}
+		else
+			lastValue = state ? 1 : 0;
 	}
 	return rc;
 }
